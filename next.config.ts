d312da2cd9b213250895b464da5fb4fb1ae8d1f6
Byek2001 <altinbekaibyek@gmail.com>
2025-03-05 12:56:31 +0800

    updates

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,26 @@
-import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const pwaConfig = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "offlineCache",
+        expiration: {
+          maxEntries: 200,
+        },
+      },
+    },
+  ],
+});
+
+const nextConfig = {
   reactStrictMode: true,
 };
 
-export default nextConfig;
+export default pwaConfig(nextConfig);
